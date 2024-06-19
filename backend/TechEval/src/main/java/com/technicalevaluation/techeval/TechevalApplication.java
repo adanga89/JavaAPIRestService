a11cb4f08181dda9d46a246be7fb91c633b8ddc4
adanga89 <47568428+adanga89@users.noreply.github.com>
2024-06-18 18:10:01 -0600

    First Commit
    Creación del Proyecto

---
+++ backend/TechEval/src/main/java/com/technicalevaluation/techeval/TechevalApplication.java
@@ -0,0 +1,13 @@
+package com.technicalevaluation.techeval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechevalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechevalApplication.class, args);
+	}
+
+}
---
+++ backend/TechEval/src/test/java/com/technicalevaluation/techeval/TechevalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicalevaluation.techeval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechevalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
